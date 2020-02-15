df13c7a861d3ca96084c08ba2988e46938aff699
jasontu <jasontu@wezhuiyi.com>
2020-02-15 21:21:51 +0800

    增加图片

--- LinearRegression/exercise1.py
+++ LinearRegression/exercise1.py
@@ -16,7 +16,7 @@ def __plot(feature, label):
 
   Args:
     feature: 特征, 维度为(样本数, 1).
-    label: 标签, 维度为(样本数, 1).
+    label: 标签, 维度为(样本数).
   """
   plt.figure()
   plt.plot(feature, label, "rx", label="Training Data")
@@ -29,8 +29,8 @@ def __cost_visualizing(feature, label, best_theta):
 
   Args:
     feature: 特征, 维度为(样本数, 特征数).
-    label: 维度为(样本数, 1).
-    best_theta: 最优参数, 维度为(n, 1).
+    label: 维度为(样本数).
+    best_theta: 最优参数, 维度为(特征数).
   """
   num = 100
   theta0_vals = np.linspace(-10, 10, num)
@@ -41,7 +41,7 @@ def __cost_visualizing(feature, label, best_theta):
   costs = np.zeros((num, num))
   for i in range(num):
     for j in range(num):
-      theta = [[grid1[i, j]], [grid2[i, j]]]
+      theta = [grid1[i, j], grid2[i, j]]
       costs[i, j] = compute_cost(feature, label, theta)
 
   plt.figure()
@@ -67,7 +67,7 @@ def __cmd():
   """
   data = np.loadtxt(Path(__file__).parent / "data1.txt",  delimiter=",")
   feature = np.expand_dims(data[:, 0], 1)
-  label = np.expand_dims(data[:, 1], 1)
+  label = data[:, -1]
   num = len(feature)  # 样本数
 
   # 绘图显示数据.
@@ -76,11 +76,11 @@ def __cmd():
   # 梯度下降.
   lr = 0.01
   iteration = 1500
-  init_theta = np.zeros((2, 1))
+  init_theta = np.zeros(2)
   feature = np.concatenate([np.ones((num, 1)), feature], axis=-1)  # 增加全为1的第0列.
   best_theta, _ = gradient_descent(feature, label, init_theta, lr, iteration)
-  logging.info(f"梯度下降得到的最优参数: [{best_theta[0, 0]:.5f} "
-               f"{best_theta[1, 0]:.5f}]")
+  logging.info(f"梯度下降得到的最优参数: [{best_theta[0]:.5f} "
+               f"{best_theta[1]:.5f}]")
 
   plt.plot(feature[:, 1], np.dot(feature, best_theta), "-",
            label="Linear Regression")
@@ -90,7 +90,7 @@ def __cmd():
   pred = np.squeeze(np.dot(np.array([[1, 35000]]), best_theta))
   logging.info(f"人口为35000时, 预测得到的利润为: {pred:.2f}")
 
-  # 代价函数可视化.
+  # # 代价函数可视化.
   __cost_visualizing(feature, label, best_theta)
   plt.show()
 
